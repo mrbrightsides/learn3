1a4715ce96bbafa47ea2cb8274ce4ae727b234d6
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-12 14:01:51 +0700

    Delete tools_mushaf.py

--- tools_mushaf.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-
-def load_mushaf(path="mushaf_mini.json"):
-    with open(Path(path), encoding="utf-8") as f:
-        return json.load(f)
-
-MUSHAF = load_mushaf()
